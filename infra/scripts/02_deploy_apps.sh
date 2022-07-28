3a04ede529919780112109e68eb719d09d625649
uturkarslan <ugurturkarslan@gmail.com>
2022-07-28 15:06:21 +0200

    Clean up

--- infra/scripts/02_deploy_apps.sh
+++ infra/scripts/02_deploy_apps.sh
@@ -19,18 +19,6 @@ kafka["namespace"]="kafka"
 kafka["port"]=9092
 kafka["topic"]="tracing"
 
-# Zipkin Server
-declare -A zipkinserver
-zipkinserver["name"]="zipkinserver"
-zipkinserver["namespace"]="zipkin"
-zipkinserver["port"]=9411
-
-# Zipkin Exporter
-declare -A zipkinexporter
-zipkinexporter["name"]="zipkinexporter"
-zipkinexporter["namespace"]="zipkin"
-zipkinexporter["port"]=8080
-
 # Proxy
 declare -A proxy
 proxy["name"]="proxy"
@@ -59,280 +47,119 @@ third["imageName"]="third-go"
 third["namespace"]="third"
 third["port"]=8080
 
-# Fourth
-declare -A fourth
-fourth["name"]="fourth"
-fourth["imageName"]="fourth-go"
-fourth["namespace"]="fourth"
-fourth["port"]=8080
-fourth["nginxName"]="nginx-fourth"
-fourth["nginxPort"]=80
-
-# Fifth
-declare -A fifth
-fifth["name"]="fifth"
-fifth["imageName"]="fifth-go"
-fifth["namespace"]="fifth"
-fifth["port"]=8080
 #########
 
-# ####################
-# ### Build & Push ###
-# ####################
+####################
+### Build & Push ###
+####################
 
-# # Zookeeper
-# echo -e "\n--- Zookeeper ---\n"
-# docker build \
-#   --platform linux/amd64 \
-#   --tag "${DOCKERHUB_NAME}/${zookeeper[name]}" \
-#   "../../apps/kafka/zookeeper/."
-# docker push "${DOCKERHUB_NAME}/${zookeeper[name]}"
-# echo -e "\n------\n"
+# Zookeeper
+docker build \
+  --tag "${DOCKERHUB_NAME}/${zookeeper[name]}" \
+  "../../apps/kafka/zookeeper/."
+docker push "${DOCKERHUB_NAME}/${zookeeper[name]}"
 
-# # Kafka
-# echo -e "\n--- Kafka ---\n"
-# docker build \
-#   --platform linux/amd64 \
-#   --tag "${DOCKERHUB_NAME}/${kafka[name]}" \
-#   "../../apps/kafka/kafka/."
-# docker push "${DOCKERHUB_NAME}/${kafka[name]}"
-# echo -e "\n------\n"
-
-# # Zipkin Exporter
-# echo -e "\n--- Zipkin Exporter ---\n"
-# docker build \
-#   --platform linux/amd64 \
-#   --tag "${DOCKERHUB_NAME}/${zipkinexporter[name]}" \
-#   "../../apps/${zipkinexporter[name]}/."
-# docker push "${DOCKERHUB_NAME}/${zipkinexporter[name]}"
-# echo -e "\n------\n"
+# Kafka
+docker build \
+  --tag "${DOCKERHUB_NAME}/${kafka[name]}" \
+  "../../apps/kafka/kafka/."
+docker push "${DOCKERHUB_NAME}/${kafka[name]}"
 
 # Proxy
-echo -e "\n--- Proxy ---\n"
 docker build \
   --tag "${DOCKERHUB_NAME}/${proxy[imageName]}" \
   "../../apps/${proxy[name]}/."
 docker push "${DOCKERHUB_NAME}/${proxy[imageName]}"
-echo -e "\n------\n"
 
-# # First
-# echo -e "\n--- First ---\n"
-# docker build \
-#   --tag "${DOCKERHUB_NAME}/${first[imageName]}" \
-#   "../../apps/${first[name]}/."
-# docker push "${DOCKERHUB_NAME}/${first[imageName]}"
-# echo -e "\n------\n"
+# First
+docker build \
+  --tag "${DOCKERHUB_NAME}/${first[imageName]}" \
+  "../../apps/${first[name]}/."
+docker push "${DOCKERHUB_NAME}/${first[imageName]}"
 
 # Second
-echo -e "\n--- Second ---\n"
 docker build \
   --tag "${DOCKERHUB_NAME}/${second[imageName]}" \
   "../../apps/${second[name]}/."
 docker push "${DOCKERHUB_NAME}/${second[imageName]}"
-echo -e "\n------\n"
 
-# # Third
-# echo -e "\n--- Third ---\n"
-# docker build \
-#   --tag "${DOCKERHUB_NAME}/${third[name]}" \
-#   "../../apps/${third[name]}/."
-# docker push "${DOCKERHUB_NAME}/${third[name]}"
-# echo -e "\n------\n"
+# Third
+docker build \
+  --tag "${DOCKERHUB_NAME}/${third[imageName]}" \
+  "../../apps/${third[name]}/."
+docker push "${DOCKERHUB_NAME}/${third[imageName]}"
 
-# # Fourth (nginx)
-# echo -e "\n--- Fourth (nginx) ---\n"
-# docker build \
-#   --platform linux/amd64 \
-#   --build-arg nginxName=${fourth[nginxName]} \
-#   --build-arg nginxPort=${fourth[nginxPort]} \
-#   --build-arg proxyPort=${fourth[port]} \
-#   --tag "${DOCKERHUB_NAME}/${fourth[nginxName]}" \
-#   "../../apps/nginx/."
-# docker push "${DOCKERHUB_NAME}/${fourth[nginxName]}"
-# echo -e "\n------\n"
+#######
 
-# # Fourth (app)
-# echo -e "\n--- Fourth (app) ---\n"
-# docker build \
-#   --platform linux/amd64 \
-#   --build-arg newRelicAppName=${fourth[name]} \
-#   --build-arg newRelicLicenseKey=$NEWRELIC_LICENSE_KEY \
-#   --tag "${DOCKERHUB_NAME}/${fourth[name]}" \
-#   "../../apps/${fourth[name]}/."
-# docker push "${DOCKERHUB_NAME}/${fourth[name]}"
-# echo -e "\n------\n"
+#############
+### Kafka ###
+#############
 
-# # Fifth
-# echo -e "\n--- Fifth ---\n"
-# docker build \
-#   --platform linux/amd64 \
-#   --build-arg newRelicAppName=${fifth[name]} \
-#   --build-arg newRelicLicenseKey=$NEWRELIC_LICENSE_KEY \
-#   --tag "${DOCKERHUB_NAME}/${fifth[name]}" \
-#   "../../apps/${fifth[name]}/."
-# docker push "${DOCKERHUB_NAME}/${fifth[name]}"
-# echo -e "\n------\n"
-# #######
+# Zookeeper
+echo "Deploying Zookeeper ..."
 
-# ################
-# ### Newrelic ###
-# ################
-# echo "Deploying Newrelic ..."
+helm upgrade ${zookeeper[name]} \
+  --install \
+  --wait \
+  --debug \
+  --create-namespace \
+  --namespace ${zookeeper[namespace]} \
+  --set dockerhubName=$DOCKERHUB_NAME \
+  --set name=${zookeeper[name]} \
+  --set namespace=${zookeeper[namespace]} \
+  --set port=${zookeeper[port]} \
+  ../charts/zookeeper
 
-# kubectl apply -f https://download.newrelic.com/install/kubernetes/pixie/latest/px.dev_viziers.yaml && \
-# kubectl apply -f https://download.newrelic.com/install/kubernetes/pixie/latest/olm_crd.yaml && \
-# helm repo add newrelic https://helm-charts.newrelic.com && helm repo update && \
-# kubectl create namespace newrelic ; helm upgrade --install newrelic-bundle newrelic/nri-bundle \
-#   --wait \
-#   --debug \
-#   --set global.licenseKey=$NEWRELIC_LICENSE_KEY \
-#   --set global.cluster=$aksName \
-#   --namespace=newrelic \
-#   --set newrelic-infrastructure.privileged=true \
-#   --set global.lowDataMode=true \
-#   --set ksm.enabled=true \
-#   --set kubeEvents.enabled=true \
-#   --set prometheus.enabled=true \
-#   --set logging.enabled=true \
-#   --set newrelic-pixie.enabled=true \
-#   --set newrelic-pixie.apiKey=$PIXIE_API_KEY \
-#   --set pixie-chart.enabled=true \
-#   --set pixie-chart.deployKey=$PIXIE_DEPLOY_KEY \
-#   --set pixie-chart.clusterName=$aksName
-# #########
+# Kafka
+echo "Deploying Kafka ..."
 
-# ##########################
-# ### Ingress Controller ###
-# ##########################
-# echo "Deploying Ingress Controller ..."
+helm upgrade ${kafka[name]} \
+  --install \
+  --wait \
+  --debug \
+  --create-namespace \
+  --namespace ${kafka[namespace]} \
+  --set dockerhubName=$DOCKERHUB_NAME \
+  --set name=${kafka[name]} \
+  --set namespace=${kafka[namespace]} \
+  --set port=${kafka[port]} \
+  ../charts/kafka
 
-# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx && \
-# helm repo update; \
-# helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
-#   --namespace nginx --create-namespace \
-#   --wait \
-#   --debug \
-#   --set controller.replicaCount=1 \
-#   --set controller.nodeSelector."kubernetes\.io/os"="linux" \
-#   --set controller.image.image="ingress-nginx/controller" \
-#   --set controller.image.tag="v1.1.1" \
-#   --set controller.image.digest="" \
-#   --set controller.service.externalTrafficPolicy=Local \
-#   --set controller.admissionWebhooks.patch.nodeSelector."kubernetes\.io/os"="linux" \
-#   --set controller.admissionWebhooks.patch.image.image="ingress-nginx/kube-webhook-certgen" \
-#   --set controller.admissionWebhooks.patch.image.tag="v1.1.1" \
-#   --set controller.admissionWebhooks.patch.image.digest="" \
-#   --set defaultBackend.nodeSelector."kubernetes\.io/os"="linux" \
-#   --set defaultBackend.image.image="defaultbackend-amd64" \
-#   --set defaultBackend.image.tag="1.5" \
-#   --set defaultBackend.image.digest=""
-# #########
+# Topic
+echo "Checking topic [${kafka[topic]}] ..."
 
-# #############
-# ### Kafka ###
-# #############
+topicExists=$(kubectl exec -n "${kafka[namespace]}" "${kafka[name]}-0" -it -- bash \
+  /kafka/bin/kafka-topics.sh \
+  --bootstrap-server "${kafka[name]}.${kafka[namespace]}.svc.cluster.local:${kafka[port]}" \
+  --list \
+  | grep ${kafka[topic]})
 
-# # Zookeeper
-# echo "Deploying Zookeeper ..."
+if [[ $topicExists == "" ]]; then
 
-# helm upgrade ${zookeeper[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace ${zookeeper[namespace]} \
-#   --set dockerhubName=$DOCKERHUB_NAME \
-#   --set name=${zookeeper[name]} \
-#   --set namespace=${zookeeper[namespace]} \
-#   --set port=${zookeeper[port]} \
-#   ../charts/zookeeper
+  echo " -> Topic does not exist. Creating ..."
+  while :
+  do
+    isTopicCreated=$(kubectl exec -n "${kafka[namespace]}" "${kafka[name]}-0" -it -- bash \
+      /kafka/bin/kafka-topics.sh \
+      --bootstrap-server "${kafka[name]}.${kafka[namespace]}.svc.cluster.local:${kafka[port]}" \
+      --create \
+      --topic ${kafka[topic]} \
+      2> /dev/null)
 
-# # Kafka
-# echo "Deploying Kafka ..."
+    if [[ $isTopicCreated == "" ]]; then
+      echo " -> Kafka pods are not fully ready yet. Waiting ..."
+      sleep 2
+      continue
+    fi
 
-# helm upgrade ${kafka[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace ${kafka[namespace]} \
-#   --set dockerhubName=$DOCKERHUB_NAME \
-#   --set name=${kafka[name]} \
-#   --set namespace=${kafka[namespace]} \
-#   --set port=${kafka[port]} \
-#   ../charts/kafka
+    echo -e " -> Topic is created successfully.\n"
+    break
 
-# # Topic
-# echo "Checking topic [${kafka[topic]}] ..."
-
-# topicExists=$(kubectl exec -n "${kafka[namespace]}" "${kafka[name]}-0" -it -- bash \
-#   /kafka/bin/kafka-topics.sh \
-#   --bootstrap-server "${kafka[name]}.${kafka[namespace]}.svc.cluster.local:${kafka[port]}" \
-#   --list \
-#   | grep ${kafka[topic]})
-
-# if [[ $topicExists == "" ]]; then
-
-#   echo " -> Topic does not exist. Creating ..."
-#   while :
-#   do
-#     isTopicCreated=$(kubectl exec -n "${kafka[namespace]}" "${kafka[name]}-0" -it -- bash \
-#       /kafka/bin/kafka-topics.sh \
-#       --bootstrap-server "${kafka[name]}.${kafka[namespace]}.svc.cluster.local:${kafka[port]}" \
-#       --create \
-#       --topic ${kafka[topic]} \
-#       2> /dev/null)
-
-#     if [[ $isTopicCreated == "" ]]; then
-#       echo " -> Kafka pods are not fully ready yet. Waiting ..."
-#       sleep 2
-#       continue
-#     fi
-
-#     echo -e " -> Topic is created successfully.\n"
-#     break
-
-#   done
-# else
-#   echo -e " -> Topic already exists.\n"
-# fi
-# #########
-
-# #############
-# ## Zipkin ###
-# #############
-
-# # Zipkin Server
-# echo "Deploying Zipkin server..."
-
-# helm upgrade ${zipkinserver[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace ${zipkinserver[namespace]} \
-#   --set dockerhubName=$DOCKERHUB_NAME \
-#   --set name=${zipkinserver[name]} \
-#   --set namespace=${zipkinserver[namespace]} \
-#   --set port=${zipkinserver[port]} \
-#   "../charts/${zipkinserver[name]}"
-
-# # Zipkin Exporter
-# echo "Deploying Zipkin exporter..."
-
-# helm upgrade ${zipkinexporter[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace ${zipkinexporter[namespace]} \
-#   --set dockerhubName=$DOCKERHUB_NAME \
-#   --set newRelicLicenseKey=$NEWRELIC_LICENSE_KEY \
-#   --set name=${zipkinexporter[name]} \
-#   --set namespace=${zipkinexporter[namespace]} \
-#   --set port=${zipkinexporter[port]} \
-#   "../charts/${zipkinexporter[name]}"
-# #########
+  done
+else
+  echo -e " -> Topic already exists.\n"
+fi
+#########
 
 #############
 ### Proxy ###
@@ -350,30 +177,30 @@ helm upgrade ${proxy[name]} \
   --set imageName=${proxy[imageName]} \
   --set namespace=${proxy[namespace]} \
   --set port=${proxy[port]} \
-  --set newRelicLicensekey=$NEWRELIC_LICENSE_KEY \
+  --set newRelicLicenseKey=$NEWRELIC_LICENSE_KEY \
   "../charts/${proxy[name]}"
 #########
 
-# #################
-# ### First app ###
-# #################
-# echo "Deploying first app..."
+#################
+### First app ###
+#################
+echo "Deploying first app..."
 
-# helm upgrade ${first[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace ${first[namespace]} \
-#   --set dockerhubName=$DOCKERHUB_NAME \
-#   --set name=${first[name]} \
-#   --set imageName=${first[imageName]} \
-#   --set namespace=${first[namespace]} \
-#   --set port=${first[port]} \
-#   "../charts/${first[name]}"
-# #########
+helm upgrade ${first[name]} \
+  --install \
+  --wait \
+  --debug \
+  --create-namespace \
+  --namespace ${first[namespace]} \
+  --set dockerhubName=$DOCKERHUB_NAME \
+  --set name=${first[name]} \
+  --set imageName=${first[imageName]} \
+  --set namespace=${first[namespace]} \
+  --set port=${first[port]} \
+  "../charts/${first[name]}"
+#########
 
-##################
+#################
 ### Second app ###
 ##################
 echo "Deploying second app..."
@@ -388,62 +215,25 @@ helm upgrade ${second[name]} \
   --set name=${second[name]} \
   --set namespace=${second[namespace]} \
   --set port=${second[port]} \
-  --set newRelicLicensekey=$NEWRELIC_LICENSE_KEY \
+  --set newRelicLicenseKey=$NEWRELIC_LICENSE_KEY \
   "../charts/${second[name]}"
 #########
 
-# #################
-# ### Third App ###
-# #################
-# echo "Deploying third app..."
+#################
+### Third App ###
+#################
+echo "Deploying third app..."
 
-# helm upgrade ${third[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace ${third[namespace]} \
-#   --set dockerhubName=$DOCKERHUB_NAME \
-#   --set name=${third[name]} \
-#   --set namespace=${third[namespace]} \
-#   --set port=${third[port]} \
-#   "../charts/${third[name]}"
-# #########
-
-# ##################
-# ### Fourth App ###
-# ##################
-# echo "Deploying fourth app..."
-
-# helm upgrade ${fourth[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace ${fourth[namespace]} \
-#   --set dockerhubName=$DOCKERHUB_NAME \
-#   --set name=${fourth[name]} \
-#   --set namespace=${fourth[namespace]} \
-#   --set port=${fourth[port]} \
-#   --set nginxName=${fourth[nginxName]} \
-#   --set nginxPort=${fourth[nginxPort]} \
-#   "../charts/${fourth[name]}"
-# #########
-
-# ##################
-# ### Fifth App ###
-# ##################
-# echo "Deploying fifth app..."
-
-# helm upgrade ${fifth[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace ${fifth[namespace]} \
-#   --set dockerhubName=$DOCKERHUB_NAME \
-#   --set name=${fifth[name]} \
-#   --set namespace=${fifth[namespace]} \
-#   --set port=${fifth[port]} \
-#   "../charts/${fifth[name]}"
-# #########
+helm upgrade ${third[name]} \
+  --install \
+  --wait \
+  --debug \
+  --create-namespace \
+  --namespace ${third[namespace]} \
+  --set dockerhubName=$DOCKERHUB_NAME \
+  --set name=${third[name]} \
+  --set namespace=${third[namespace]} \
+  --set port=${third[port]} \
+  --set newRelicLicenseKey=$NEWRELIC_LICENSE_KEY \
+  "../charts/${third[name]}"
+#########
